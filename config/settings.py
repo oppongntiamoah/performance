3cff2373323c570922198c07f1bd84a79c1b46e1
Emmanuel Oppong Ntiamoah <ntiamoahone@gmail.com>
2025-09-09 14:38:54 +0000

    gd

--- config/settings.py
+++ config/settings.py
@@ -85,16 +85,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',  
+#         'NAME': 'veep',        
+#         'USER': 'postgres',     
+#         'PASSWORD': 'admin',  
+#         'HOST': 'localhost',    
+#         'PORT': '5432',         
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'veep',        
-        'USER': 'postgres',     
-        'PASSWORD': 'admin',  
-        'HOST': 'localhost',    
-        'PORT': '5432',         
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Database file will be created in your project root
     }
 }
+
 
 
 
@@ -136,6 +145,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
